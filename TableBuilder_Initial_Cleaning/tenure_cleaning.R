d17d32b9bcf9704ef07d0c8a9b8a22c41172ad94
of2 <owen.forbes@live.com>
2023-02-20 20:57:17 +1100

    Applying cleaning pipeline; Added code/name matching files; testing pipeline on Religion and tenure type
    Still need to add code/name functionality to the base cleaning function

--- TableBuilder_Initial_Cleaning/tenure_cleaning.R
+++ TableBuilder_Initial_Cleaning/tenure_cleaning.R
@@ -1,4 +1,4 @@
-# Cleaning code for TableBuilder Data - CENSUS
+# Cleaning code for TableBuilder Data - CENSUS - Tenure Type
 
 library(tidyverse)
 library(readr)
@@ -22,7 +22,6 @@ TB_Census_cleaning_fn <- function(data_file_base, data_item_name, calendar_year,
   for(i in 1:length(geog_list)){ #loop over the 6 geography types
     
     data_temp_name <- paste0(data_file_base,"_",geog_list[i],"_",calendar_year,".csv")
-    
     data_temp <- read.csv(data_temp_name,skip=8,row.names=NULL,na.strings=c("","NA"), check.names=FALSE)  
     #Skips first 10 ("8") lines -- what read.csv sees as the size of the  header from TableBuilder export.
     #row.names=NULL was due to an error I was getting - due to mix of tab/comma delimitation in outputs... workaround below
@@ -32,30 +31,31 @@ TB_Census_cleaning_fn <- function(data_file_base, data_item_name, calendar_year,
     #issue with header formatting tabs vs commas... need following lines to fix up colnames
     colnames(data_temp) <- colnames(data_temp)[2:ncol(data_temp)] #fix colnames issue
     data_temp <- data_temp[,1:(ncol(data_temp)-1)] #drop added NA column
+    colnames(data_temp)[ncol(data_temp)] <- data_item_name
     
-    colnames(data_temp)[ncol(data_temp)] <- data_item_name #rename variable column with specified data_item_name
     
     # delete trailing crap rows
-    trailing_rows <- which(is.na(data_temp[,ncol(data_temp)])) #identify NA's in final column (variable of interest)
+    trailing_rows <- which(is.na(data_temp[,ncol(data_temp)]))
     data_temp <- data_temp[-trailing_rows,] #deleting trailing rows (4)
     
     
     # fill down vars
+    
     data_temp <- data_temp %>% fill(everything(),.direction="down")
     
     # check column names
+    
     names(data_temp)
     
     
-    # If geog type = name, then  sub in code for name...
+    
+    # If geog type = name, then  sub in code for name
     
     #1. find geo type - geog_list[i]
     #2. find geo column 
     if(code_or_name == "name"){
       geo_column_index <- which(grepl(geog_list[i],names(data_temp))) #What column is the relevant geom?
-      #data_temp[,geo_column_index] <- XXXXYYYYZZZ_FINISH_THIS_FUNCTION!!!!!
-      
-      #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+      #data_temp[,geo_column_index] <- XXXXYYYYZZZ_FINISH_THIS_FUNCTION
     }
     
     
@@ -69,11 +69,47 @@ TB_Census_cleaning_fn <- function(data_file_base, data_item_name, calendar_year,
 }
 
 
-# TEST
 
-## Set WD to raw data files
 
-#setwd("/Users/Current/OneDrive - Queensland University of Technology/ANCHDA_QUT/Data_Collections_RAW/public_data/TableBuilder_Data/Census/Census_2006")
+#-------------------
 
-#TB_Census_cleaning_fn(data_file_base = "census_year12", data_item_name ="Completed_Year12", calendar_year=2006)
+# APPLY to Religion
+
+#2006
+
+setwd("/Users/Current/OneDrive - Queensland University of Technology/ANCHDA_QUT/Data_Collections_RAW/public_data/TableBuilder_Data/Census/Census_2006")
+
+TB_Census_cleaning_fn(data_file_base = "census_tenure_type", data_item_name ="Dwelling_Tenure_Type", calendar_year=2006,code_or_name = "code")
+
+#------
+
+
+#2011
+
+setwd("/Users/Current/OneDrive - Queensland University of Technology/ANCHDA_QUT/Data_Collections_RAW/public_data/TableBuilder_Data/Census/Census_2011")
+
+TB_Census_cleaning_fn(data_file_base = "census_tenure_type", data_item_name ="Dwelling_Tenure_Type", calendar_year=2011,code_or_name = "code")
+
+
+#-----
+
+
+#2016
+
+setwd("/Users/Current/OneDrive - Queensland University of Technology/ANCHDA_QUT/Data_Collections_RAW/public_data/TableBuilder_Data/Census/Census_2016")
+
+TB_Census_cleaning_fn(data_file_base = "census_tenure_type", data_item_name ="Dwelling_Tenure_Type", calendar_year=2016,code_or_name = "code")
+
+
+#------
+
+
+#2021
+
+setwd("/Users/Current/OneDrive - Queensland University of Technology/ANCHDA_QUT/Data_Collections_RAW/public_data/TableBuilder_Data/Census/Census_2021")
+
+TB_Census_cleaning_fn(data_file_base = "census_tenure_type", data_item_name ="Dwelling_Tenure_Type", calendar_year=2021,code_or_name = "code")
+
+
+
 
