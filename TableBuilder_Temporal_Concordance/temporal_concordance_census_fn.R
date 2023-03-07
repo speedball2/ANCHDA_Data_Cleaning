0af27295e48d99c7483c87618a20dcc62673033c
of2 <owen.forbes@live.com>
2023-03-07 21:12:57 +1100

    added new temporal correspondence files; planning correspondence pipeline

--- TableBuilder_Temporal_Concordance/temporal_concordance_census_fn.R
+++ TableBuilder_Temporal_Concordance/temporal_concordance_census_fn.R
@@ -2,11 +2,28 @@
 
 
 library(tidyverse) #for tidyr::fill()
+library(readxl)
 
-# Read in all census years
+SLA_2006_SA2_2016 <- read_excel()
+SSD_2006_SA3_2016 <- 
+SD_2006_SA4_2016 <- 
+  
+
+SA2_2011_SA2_2016 <- 
+SA3_2011_SA3_2016 <- 
+SA4_2011_SA4_2016 <- 
+  
+SA2_2021_SA2_2016 <- 
+SA3_2021_SA3_2016 <- 
+SA4_2021_SA4_2016 <- 
+
+# FOR ONE VARIABLE
+# Read in all census years for one geography
+
+# Read in correspondence files for that geography
 
 # Convert each year 
-# 2006 --> 2011 --> 2016
+# 2006 --> 2016
 # 2011 --> 2016
 # 2016 <-- 2021
 
@@ -15,5 +32,10 @@ library(tidyverse) #for tidyr::fill()
 temporal_concordance_census_fn <- function(data_file_base, data_item_name){
   
   
+  
+  
+  # Read in correspondence files for that geography
+  
+  if (geog_list[i]) = 
   
 }
