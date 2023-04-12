aa753aacb5b23c231529a403dafe7a00fd71ae47
hazardously <harriette.phillips@hdr.qut.edu.au>
2023-04-12 11:46:47 +1000

    FIXING GITHUB

---
+++ custodian_data/BOSCAR.R
@@ -0,0 +1,152 @@
+
+
+# Harriette's WD
+
+setwd("C:/Users/n9955348/OneDrive - Queensland University of Technology/Shared Documents - ACWA_QUT/General/Data_Collections_RAW/from_custodians/BOSCAR_SA4_LGA")
+
+
+
+# TO DO ------------------------------------------------------------------------
+
+# what to do w/ unknown/missing data for m/f
+
+# libraries --------------------------------------------------------------------
+
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+# cleaning function ------------------------------------------------------------
+
+cleaning <- function(path, sht, range){
+  #path = file path, sht = Sheet number, range = column range 
+  #code = SA4/LGA region for pivoting wide to long
+  
+  #READING IN SPREADSHEETS FROM EXCEL
+  df <- as.data.frame(read_xlsx(path,
+           sht,
+           range,
+           col_names = T))
+  #REMOVING 1ST COL (BLANK)
+  
+  df <- df[-1,]
+  
+  # REMOVING ATSI STATUS
+  
+  df <- select(df, -c (Aboriginality))
+  
+  
+ # CONCISTENT NAMING CONVENTIONS W/ DATA DICTIONARY
+  
+  df[df[,] == 0] <- NA
+  df[df[,] == "1 to 4"] <- "<5"
+  
+  
+  #RENAMING COLUMNS FOR ALL DFS
+  names(df)[names(df) == "Age of victim (in years)"] <- "age_group"
+  names(df)[names(df) == "Gender"] <- "sex"
+  names(df)[names(df) == "SA4 of Incident"] <- "SA4_NAME16"
+  names(df)[names(df) == "LGA of Incident"] <- "LGA_NAME16"
+
+
+  # REMOVING RANDOM "Y" FROM DFs
+# df 8 has a 'y' at the end of the age column
+  
+  df$age_group <-gsub("y","",as.character(df$age_group))
+  
+  
+  # PIVOTING DATA FROM WIDE TO LONG
+  df <- gather(df, calendar_year, indicator, gathercol <- c("2006", "2007", "2008", "2009","2010","2011","2012", "2013","2014","2015","2016","2017", "2018","2019","2020","2021")) 
+  
+  #DF = DATA FRAME 
+  #INDICATOR = COUNTS FOR RATES IN NEW COL (a new col)
+  #CALENDAR_YEAR = NEW COLUMN FOR "GATHERCOL" VARIABLES (a new col)
+  #OTHER COLUMNNS FOLLOW
+ 
+return(df)
+   
+}
+
+# SPECIFIC TO EACH DF ----------------------------------------------------------
+
+df1 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 1, "A5:T415")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df1)[names(df1) == "indicator"] <- "victims_domestic_violence_related_assault"
+
+# ------------------------------------------------------------------------------
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+df2 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 2, "A5:T1571")
+
+names(df2)[names(df2) == "indicator"] <- "victims_domestic_violence_related_assault"
+
+# ------------------------------------------------------------------------------
+
+
+df3 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 3, "A5:T90")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df3)[names(df3) == "indicator"] <- "victims_domestic_violence_related_murder"
+
+# ------------------------------------------------------------------------------
+
+df4 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 4, "A5:T114")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df4)[names(df4) == "indicator"] <- "victims_domestic_violence_related_murder"
+
+# ------------------------------------------------------------------------------
+
+df5 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 5, "A5:T393")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df5)[names(df5) == "indicator"] <- "victims_sexual_assault"
+
+# ------------------------------------------------------------------------------
+
+df6 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 6, "A5:T1325")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df6)[names(df6) == "indicator"] <- "victims_sexual_assault"
+
+# ------------------------------------------------------------------------------
+
+df7 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 7, "A5:T392")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df7)[names(df7) == "indicator"] <- "victims_sexual_touching"
+
+# ------------------------------------------------------------------------------
+
+df8 <- cleaning("ab23-22205 Victims by Aboriginality Gender SA4 LGA.xlsx", 8, "A5:T1337")
+
+#RENAME INDICATOR COLUMN TO BE SHEET SPECIFIC
+names(df8)[names(df8) == "indicator"] <- "victims_sexual_touching"
+
+
+# reading in geographies for codes ---------------------------------------------
+
+sa4 <- read.csv("SA4_2016_AUST 2.csv")
+
+sa4 <- sa4[,-(3:7)]
+
+test <- as.data.frame(df1[,3])
+
+#print(match(test, sa4)) this but for the name cols 
+
+test2 <- 
+
+  
+  # ../ from where R script is saved 
+  lga <- read_xlsx("LGA_2021_AUST (1).xlsx", 1, "A1:C90000", T)
+ lga2 <- read_xlsx("LGA_2021_AUST (1).xlsx", 1, "A90001:C100000", T)
+
+ lga <- read_xlsx("LGA_2021_AUST (1).xlsx", 1, cell_limits(c(1, 1), c(NA, 3)), T)
+
+ lga <- lga[,-1]
+ 
+
+lga <- lga[!duplicated(lga),]
+
+print(getwd())
---
+++ custodian_data/ERP.R
@@ -0,0 +1,70 @@
+
+#Harriette's WD
+setwd("C:/Users/n9955348/OneDrive - Queensland University of Technology/Shared Documents - ACWA_QUT/General/Data_Collections_RAW/from_custodians/ABS_ERP")
+
+
+# ----------------- #
+# --- libraries --- #
+# ----------------- #
+
+library(readxl)
+library(tidyr)
+
+
+# READING IN EXCEL 
+df1 <- read_xlsx("Client File-ERP-LS005201.xlsx", 2, "A9:BA36681", T)
+
+# REMOVING SA NAME 
+df1 <- df1[,-3]
+
+#RENAME COLUMNS 
+names(df1)[1] <- "calendar_year"
+names(df1)[2] <- "SA2_CODE16"
+
+#SPLITTING GENDERS
+i <- c("SA2_CODE16", "calendar_year")
+m <- c("m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15",
+        "m16", "m17", "m18", "m19", "m20", "m21", "m22", "m23", "m24"  )
+
+f <- c("f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
+        "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16", "f17",
+        "f18", "f19", "f20", "f21", "f22", "f23", "f24"  )
+
+fem <- df1[,c(i,f)]
+mal <- df1[,c(i,m)]
+
+
+# CREATING AGE COLUMN/COMBINING (WIDE TO LONG DATA)
+
+#REMOVING LEADING LETTER FOR GENDER 
+colnames(mal)<-gsub("m","",colnames(mal))
+colnames(fem)<-gsub("f","",colnames(fem))
+
+#COMBINING AGE COLUMNS INTO 1
+mal2 <- gather(mal, age_group, estimated_regional_population, gathercol <-  c("0", "1", "2", "3", "4", "5", "6", "7", "8",
+                                                                              "9", "10", "11", "12", "13", "14", "15", "16", "17",
+                                                                              "18", "19", "20", "21", "22", "23", "24"))
+#ADDING sSEX COLUMN MALE
+mal2$sex <- "male"
+
+#COMBINING AGE COLUMNS INTO 1
+fem2 <- gather(fem, age_group, estimated_regional_population, gathercol <-  c("0", "1", "2", "3", "4", "5", "6", "7", "8",
+                                                                              "9", "10", "11", "12", "13", "14", "15", "16", "17",
+                                                                              "18", "19", "20", "21", "22", "23", "24"))
+
+#ADDING sSEX COLUMN FEMALE
+fem2$sex <- "female"
+
+#MERGE M/F BACK TOGETHER 
+full <- rbind(fem2,mal2)
+
+#CHANGE COLORDER
+
+corder <- c("SA2_CODE16","calendar_year", "age_group", "sex", "estimated_regional_population")
+
+# write csv --------------------------------------------------------------------
+
+
+write.csv(full, "../../../Data_Collections_INTERIM/ABS_ERP_181_ERP_SA2.csv", row.names = F)
+
+
