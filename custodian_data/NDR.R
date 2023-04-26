8a6e16ecdd3b8d792d257dd60dfb6022c3945049
hazardously <harriette.phillips@hdr.qut.edu.au>
2023-04-26 14:53:41 +1000

    spelling changed, m/f capitals removed

--- custodian_data/NDR.R
+++ custodian_data/NDR.R
@@ -20,10 +20,10 @@ df1 <- read_excel(col_names = T, path = "NDR Type 1 incidence and prevalence by 
                   range = "A1:E4258")
 
 #naming columns ----------------------------------------------------------------
-colnames(df1) <- c("calendar_year","SA4_NAME16","SA4_CODE16","sex","diabetes_indicence_population")
+colnames(df1) <- c("calendar_year","SA4_NAME16","SA4_CODE16","sex","diabetes_incidence_population")
 
 #reordering columns ------------------------------------------------------------
-col_order <- c("SA4_CODE16","calendar_year","sex","diabetes_indicence_population")
+col_order <- c("SA4_CODE16","calendar_year","sex","diabetes_incidence_population")
 
 df1 <- df1[,col_order]
 
@@ -37,12 +37,12 @@ df2 <- read_excel(col_names = T, path = "NDR Type 1 incidence and prevalence by 
                   range = "A1:F24031")
 
 #naming columns ----------------------------------------------------------------
-colnames(df2) <- c("calendar_year","SA4_NAME16","SA4_CODE16","age_group","sex","diabetes_prevalance_population")
+colnames(df2) <- c("calendar_year","SA4_NAME16","SA4_CODE16","age_group","sex","diabetes_prevalence_population")
 
 
 #reordering columns ------------------------------------------------------------
 
-col_order2 <- c("SA4_CODE16","calendar_year","sex","age_group","diabetes_prevalance_population")
+col_order2 <- c("SA4_CODE16","calendar_year","sex","age_group","diabetes_prevalence_population")
 
 df2 <- df2[,col_order2] 
 
@@ -53,17 +53,26 @@ df2 <- df2[,col_order2]
 full <- merge(df1,df2,by = intersect(names(df1), names(df2)), all.x = T)
 
 
-col_order <- c("SA4_CODE16","calendar_year","sex","age_group","diabetes_prevalance_population", "diabetes_indicence_population")
+col_order <- c("SA4_CODE16","calendar_year","sex","age_group","diabetes_prevalence_population", "diabetes_incidence_population")
 full <- full[,col_order]
 
 
-# REMOVE TOTALS ----------------------------------------------------------------
+# CONCISTENCY ACROSS DATASETS ----------------------------------------------------------------
 
 # REMOVE TOTAL COLS
 
 full <- full[!grepl("Total", full$age_group),]
 
 full <- full[!grepl("Persons", full$sex),]
+
+#REMOVING CAPITALS FOR M/F
+
+if("sex" %in% names(full)){
+  full$sex <- recode(full$sex,
+                   "Female" = "female",
+                   "Male" = "male",)
+  
+  }
 
 #---------------------------#
 #--------write csv----------#
