f7f0240bd5669feac1d9b57987c7c177bb61ed71
hazardously <harriette.phillips@hdr.qut.edu.au>
2023-05-03 15:38:24 +1000

    adding sex filter col, year range full no.s

--- harriette_cleaning_code/SHSC.R
+++ harriette_cleaning_code/SHSC.R
@@ -26,9 +26,15 @@ df1 <-read_excel(col_names = F, path = "Request 4958 SHSC ANCHDA Atlas ad hoc (1
                  sheet = 2,
                  range = "A3:F20106")
 
-colnames(df1) <- c("SA3_CODE16","SA3_NAME16","age_group","sex", "year_range", "client_count_SHSC")
+#CHANING COLUMN NAMES
+colnames(df1) <- c("SA3_CODE16","SA3_NAME16", "age_group","sex", "year_range", "client_count_SHSC")
 
+#UPDATING COLUMNS
 df1$SA3_NAME16 <- NULL
+
+#REORDING COLUMNS
+corder <-  c("SA3_CODE16","year_range", "age_group", "sex", "client_count_SHSC")
+df1 <- df1[,corder]
 
 #--------------------------------------------------#
 #--------S-HSC_presenting_unit_type (df2)----------#
@@ -38,15 +44,28 @@ df2 <- read_excel(col_names = F, path = "Request 4958 SHSC ANCHDA Atlas ad hoc (
                   sheet = 3,
                   range = "A3:F60314")
 
+#CHANGING COLUMN NAMES
 colnames(df2) <- c("SA3_CODE16","SA3_NAME16","age_group", "year_range", "presenting_unit_type_SHSC", "client_count_SHSC")
 
+#UPDATING COLUMNS
 df2$SA3_NAME16 <- NULL
+df2$sex <- "all"
+
+#CHANGING COLUMN ORDER
+corder <-  c("SA3_CODE16","year_range", "age_group", "sex", "presenting_unit_type_SHSC", "client_count_SHSC")
+df2 <- df2[,corder]
+
+
+#CHANGING CELL VALUES TO A BETTER FORMAT
 
 df2["presenting_unit_type_SHSC"][df2["presenting_unit_type_SHSC"] == "Alone/not part of family"] <- "alone or not apart of a family"
 df2["presenting_unit_type_SHSC"][df2["presenting_unit_type_SHSC"] == "Couple with child/ren"] <- "couple with one or more children"
 df2["presenting_unit_type_SHSC"][df2["presenting_unit_type_SHSC"] == "Single with child/ren"] <- "couple with one or more children"
 
+#CHANGING TO LOWER CASE FOR CONCISTENCY
 df2$presenting_unit_type_SHSC <- tolower(df2$presenting_unit_type_SHSC)
+
+
 
 
 #-------------------------------------------------------------#
@@ -57,11 +76,18 @@ df3 <- read_excel(col_names = F, path = "Request 4958 SHSC ANCHDA Atlas ad hoc (
                   sheet = 4,
                   range = "A3:F60314")
 
+#CHANGING COLUMN NAMES
 colnames(df3) <- c("SA3_CODE16","SA3_NAME16","age_group", "year_range", "main_reasons_seeking_assistance_SHSC", "client_count_SHSC")
 
+#UPDATING COLUMNS
 df3$SA3_NAME16 <- NULL
+df3$sex <- "all"
 
+#CHANGING COLUMN ORDER
+corder <-  c("SA3_CODE16","year_range", "age_group", "sex", "main_reasons_seeking_assistance_SHSC", "client_count_SHSC")
+df3 <- df3[,corder]
 
+#LOWERCASE FOR CONCISTENCY
 df3$main_reasons_seeking_assistance_SHSC <- tolower(df3$main_reasons_seeking_assistance_SHSC)
 
 
@@ -81,6 +107,10 @@ cleaning <- function(df){
 
   
   df <- df[!grepl("Total", df$age_group),]
+  
+  
+  df <- mutate(df, year_range = gsub("(\\d{4})-(\\d{2})", "\\1-20\\2", year_range))
+  
   
   return(df)
   
