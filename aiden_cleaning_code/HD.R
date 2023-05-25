08e5dc6b0a5268c45936ba3020d7ca94a927a9bf
hazardously <harriette.phillips@hdr.qut.edu.au>
2023-05-25 16:06:56 +1000

    updating Aiden's file, see file for note's - H

--- aiden_cleaning_code/HD.R
+++ aiden_cleaning_code/HD.R
@@ -3,27 +3,35 @@
 ################################################################################
 
 
+#Harriette Edit 25.05.23
+# Updating Column Names
+# Updating Csv names (from interim folder and output csvs)
+# adding root dir
+# change latitude and longitude to be leading column (original order of column)
+# output of new csvs
 
 
 
 # ------------------------------------------------------------------------------
 # Save and Load Directories:
 # Change root directory to appropriate file path.
-root.dir <- "C:/Users/Mudki/OneDrive - Queensland University of Technology/ACWA_QUT/"
+#root.dir <- "C:/Users/Mudki/OneDrive - Queensland University of Technology/ACWA_QUT/"
+
+#Harriette changing names - new root.dir
+root.dir <- "C:/Users/n9955348/OneDrive - Queensland University of Technology/Shared Documents - ACWA_QUT/General/"
 file.dir <- "Data_Collections_INTERIM/Service Location Data/NHSD/"
 save.dir <- "Data_Collections_READY_FOR_QA/Service Location Data/"
 
 
 
 
-
 # ------------------------------------------------------------------------------
 # General Practice Locations:
-df1 <- read.csv(paste0(root.dir, file.dir, "NHSD_712_general_practices_service_locations.csv"))[,c(5,3,1,2,4)]
-names(df1) <- c("ID", "Service_Name", "Latitude", "Longitude", "Healthcare_Service_Type")
-# ID: healthcare_service_identifier_nhsd
+df1 <- read.csv(paste0(root.dir, file.dir, "NHSD_712_general_practice_locations.csv"))[,c(1,2,3,4,5)]
+names(df1) <- c("latitude", "longitude","organisation_name_nhsd", "healthcare_service_type_nhsd", "healthcare_service_identifier_nhsd")
+# healthcare_service_identifier_nhsd: healthcare_service_healthcare_service_identifier_nhsdentifier_nhsd
 write.csv(df1,
-          paste0(root.dir,save.dir,"NHSD_712_general_practice_service_locations.csv"),
+          paste0(root.dir,save.dir,"NHSD_712_general_practice_locations.csv"),
           row.names=F)
 
 
@@ -32,9 +40,9 @@ write.csv(df1,
 
 # ------------------------------------------------------------------------------
 # Healthcare Service Locations:
-df2 <- read.csv(paste0(root.dir, file.dir, "NHSD_713_healthcare_services_locations.csv"))[,c(5,3,1,2,4)]
-names(df2) <- c("ID", "Service_Name", "Latitude", "Longitude", "Healthcare_Service_Type")
-# ID: healthcare_service_identifier_nhsd
+df2 <- read.csv(paste0(root.dir, file.dir, "NHSD_713_healthcare_service_locations.csv"))[,c(1,2,3,4,5)]
+names(df2) <- c("latitude", "longitude","organisation_name_nhsd", "healthcare_service_type_nhsd", "healthcare_service_identifier_nhsd")
+# healthcare_service_identifier_nhsd: healthcare_service_healthcare_service_identifier_nhsdentifier_nhsd
 write.csv(df2,
           paste0(root.dir,save.dir,"NHSD_713_healthcare_service_locations.csv"),
           row.names=F)
@@ -45,25 +53,17 @@ write.csv(df2,
 
 # ------------------------------------------------------------------------------
 # Hospital Service Locations:
-df3 <- read.csv(paste0(root.dir, file.dir, "NHSD_711_hospitals_service_locations_new.csv"))[,c(5,3,1,2,4)]
-names(df3) <- c("ID", "Service_Name", "Latitude", "Longitude", "Healthcare_Service_Type")
-# ID: healthcare_service_identifier_nhsd
+df3 <- read.csv(paste0(root.dir, file.dir, "NHSD_711_hospital_service_locations.csv"))[,c(1,2,3,4,5)]
+names(df3) <- c("latitude", "longitude","organisation_name_nhsd", "healthcare_service_type_nhsd", "healthcare_service_identifier_nhsd")
+# healthcare_service_identifier_nhsd: healthcare_service_healthcare_service_identifier_nhsdentifier_nhsd
 
 df4 <- read.csv(paste0(root.dir, file.dir, "Hospitals_formatted.csv"))
-df3$Latitude <- df4$Y
-df3$Longitude <- df4$X
+df3$latitude <- df4$Y
+df3$longitude <- df4$X
 
 write.csv(df3,
           paste0(root.dir,save.dir,"NHSD_711_hospital_service_locations.csv"),
           row.names=F)
-
-
-
-
-
-
-
-
 
 
 # ------------------------------------------------------------------------------
