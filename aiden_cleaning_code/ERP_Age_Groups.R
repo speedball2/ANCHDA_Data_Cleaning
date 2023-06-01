4d534dcd9c3121d7852017fcdcdee21bc705e4f2
hazardously <harriette.phillips@hdr.qut.edu.au>
2023-06-01 12:16:26 +1000

    creating totals

--- aiden_cleaning_code/ERP_Age_Groups.R
+++ aiden_cleaning_code/ERP_Age_Groups.R
@@ -1,4 +1,14 @@
-root.dir <- "C:/Users/pricea4/OneDrive - Queensland University of Technology/ACWA_QUT/"
+
+#Harriette edits 01.05.23
+
+#adding root.dir
+#creating totals - sa2, 3, 4, STE, aus
+#library dplyr
+
+
+#root.dir <- "C:/Users/pricea4/OneDrive - Queensland University of Technology/ACWA_QUT/"
+
+root.dir <- "C:/Users/n9955348/OneDrive - Queensland University of Technology/Shared Documents - ACWA_QUT/General/"
 file.path <- "Data_Collections_INTERIM/ERP/"
 save.path <- "Data_Collections_READY_FOR_QA/ERP/"
 
@@ -10,6 +20,7 @@ library(ASGS)
 library(tidyverse)
 library(sf)
 library(readr)
+library(dplyr)
 
 df.base <- st_drop_geometry(st_as_sf(SA2_2016))
 names(df.base)[2] <- "SA2_CODE16"
@@ -58,16 +69,16 @@ df.erp.ag <- df.erp %>%
 df.new.ag <- inner_join(df.erp.ag,df.base)
 
 df.ag <- inner_join(aggregate(df.new.ag$estimated_regional_population,
-                    by=list(SA2_CODE16=df.new.ag$SA2_CODE16,
-                            calendar_year=df.new.ag$calendar_year,
-                            age_group=df.new.ag$ag,
-                            sex=df.new.ag$sex), FUN=sum),df.base)
+                              by=list(SA2_CODE16=df.new.ag$SA2_CODE16,
+                                      calendar_year=df.new.ag$calendar_year,
+                                      age_group=df.new.ag$ag,
+                                      sex=df.new.ag$sex), FUN=sum),df.base)
 
 df.ag.SA3 <- aggregate(df.ag$x,
-                    by=list(SA3_CODE16=df.ag$SA3_CODE16,
-                            calendar_year=df.ag$calendar_year,
-                            age_group=df.ag$age_group,
-                            sex=df.ag$sex), FUN=sum)
+                       by=list(SA3_CODE16=df.ag$SA3_CODE16,
+                               calendar_year=df.ag$calendar_year,
+                               age_group=df.ag$age_group,
+                               sex=df.ag$sex), FUN=sum)
 
 df.ag.SA4 <- aggregate(df.ag$x,
                        by=list(SA4_CODE16=df.ag$SA4_CODE16,
@@ -94,33 +105,78 @@ df.ag.AUS <- cbind(Australia = as.factor(0), df.ag.AUS)
 df.SA2 <- df.new[,1:5]
 df.ag.SA2 <- df.ag[,1:5]
 
+#SA2 ---------------------------------------------------------------------------
+
 erp.SA2 <- rbind(df.SA2,df.ag.SA2)
+
+#Harriette/Aiden edit: Creating Totals (01/06/23)
+
+erp.totalage.SA2 <- erp.SA2 %>% group_by(SA2_CODE16, calendar_year, age_group) %>%
+  summarise(x=sum(x)) %>% mutate(sex = "all")
+erp.totalage.SA2 <-  erp.totalage.SA2[,c(1,2,3,5,4)]
+
+erp.SA2 <- rbind(erp.SA2, erp.totalage.SA2)
+erp.SA2 <- erp.SA2[with(erp.SA2, order(sex, age_group, calendar_year, SA2_CODE16)),]
 names(erp.SA2)[5] <- "estimated_regional_population"
-write.csv(erp.SA2,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA2.csv"))
+
+write.csv(erp.SA2,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA2.csv"), row.names=F)
+
+# SA3 --------------------------------------------------------------------------
 
 erp.SA3 <- rbind(df.SA3,df.ag.SA3)
+
+#Harriette/Aiden edit: Creating Totals (01/06/23)
+
+erp.totalage.SA3 <- erp.SA3 %>% group_by(SA3_CODE16, calendar_year, age_group) %>%
+  summarise(x=sum(x)) %>% mutate(sex = "all")
+erp.totalage.SA3 <-  erp.totalage.SA3[,c(1,2,3,5,4)]
+
+erp.SA3 <- rbind(erp.SA3, erp.totalage.SA3)
+erp.SA3 <- erp.SA3[with(erp.SA3, order(sex, age_group, calendar_year, SA3_CODE16)),]
 names(erp.SA3)[5] <- "estimated_regional_population"
-write.csv(erp.SA3,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA3.csv"))
+
+names(erp.SA3)[5] <- "estimated_regional_population"
+write.csv(erp.SA3,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA3.csv" ), row.names = F)
+
+#SA4 ---------------------------------------------------------------------------
 
 erp.SA4 <- rbind(df.SA4,df.ag.SA4)
+
+erp.totalage.SA4 <- erp.SA4 %>% group_by(SA4_CODE16, calendar_year, age_group) %>%
+  summarise(x=sum(x)) %>% mutate(sex = "all")
+erp.totalage.SA4 <-  erp.totalage.SA4[,c(1,2,3,5,4)]
+
+erp.SA4 <- rbind(erp.SA4, erp.totalage.SA4)
+erp.SA4 <- erp.SA4[with(erp.SA4, order(sex, age_group, calendar_year, SA4_CODE16)),]
 names(erp.SA4)[5] <- "estimated_regional_population"
-write.csv(erp.SA4,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA4.csv"))
 
+write.csv(erp.SA4,paste0(root.dir,save.path,"ABS_ERP_181_ERP_SA4.csv" ), row.names = F)
+
+#STE ---------------------------------------------------------------------------
 erp.STE <- rbind(df.STE,df.ag.STE)
+
+erp.totalage.STE <- erp.STE %>% group_by(STE_CODE16, calendar_year, age_group) %>%
+  summarise(x=sum(x)) %>% mutate(sex = "all")
+erp.totalage.STE <-  erp.totalage.STE[,c(1,2,3,5,4)]
+
+erp.STE <- rbind(erp.STE, erp.totalage.STE)
+erp.STE <- erp.STE[with(erp.STE, order(sex, age_group, calendar_year, STE_CODE16)),]
 names(erp.STE)[5] <- "estimated_regional_population"
-write.csv(erp.STE,paste0(root.dir,save.path,"ABS_ERP_181_ERP_STE.csv"))
 
+write.csv(erp.STE,paSTE0(root.dir,save.path,"ABS_ERP_181_ERP_STE.csv" ), row.names = F)
+
+# AUS --------------------------------------------------------------------------
 erp.AUS <- rbind(df.AUS,df.ag.AUS)
+
+erp.totalage.AUS <- erp.AUS %>% group_by(Australia, calendar_year, age_group) %>%
+  summarise(x=sum(x)) %>% mutate(sex = "all")
+erp.totalage.AUS <-  erp.totalage.AUS[,c(1,2,3,5,4)]
+
+erp.AUS <- rbind(erp.AUS, erp.totalage.AUS)
+erp.AUS <- erp.AUS[with(erp.AUS, order(sex, age_group, calendar_year, Australia)),]
 names(erp.AUS)[5] <- "estimated_regional_population"
-write.csv(erp.AUS,paste0(root.dir,save.path,"ABS_ERP_181_ERP_Australia.csv"))
 
-
-
-
-
-
-
-
+write.csv(erp.AUS,paste0(root.dir,save.path,"ABS_ERP_181_ERP_Australia.csv" ), row.names = F)
 
 
 
